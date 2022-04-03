a649847f1fd4746b32b2f83f3be223f9f6cbc533
Gabriel K <96412510+RdqZ@users.noreply.github.com>
2022-04-03 14:54:33 +0000

    Create essentials.ps1

---
+++ essentials.ps1
@@ -0,0 +1,13 @@
+# Install Chocolatey
+Set-ExecutionPolicy Bypass
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) -ErrorAction Stop;
+
+choco install -y insomnia-rest-api-client
+# Install nvm from https://github.com/coreybutler/nvm-windows/releases
+choco install -y jetbrainstoolbox
+choco install -y git.install --params '"/GitOnlyOnPath /NoShellIntegration"'    
+choco install -y php --params '"/ThreadSafe /InstallDir:C:\PHP"'
+choco install -y composer ngrok
+choco pin add --name composer
+choco install -y apache-httpd --params '"/installLocation:C:\Apache"'
+msiexec.exe /i https://awscli.amazonaws.com/AWSCLIV2.msi
